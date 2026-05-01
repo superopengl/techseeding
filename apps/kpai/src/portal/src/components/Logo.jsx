af43c8f0390de1e8eda8d7413601f3ecc6d697e4
Jun Shao <mr.shaojun@gmail.com>
2026-05-01 14:57:25 +1000

    add Logo component and favicon/logo assets

---
+++ apps/kpai/src/portal/src/components/Logo.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export function Logo({ size = 32, style }) {
+  const padding = Math.round(size * 0.15);
+  const outerSize = size + padding * 2;
+  return (
+    <div
+      style={{
+        width: outerSize,
+        height: outerSize,
+        background: "#001122",
+        borderRadius: outerSize * 0.24,
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexShrink: 0,
+        ...style,
+      }}
+    >
+      <img
+        src="/logo.png"
+        alt="Lab67"
+        style={{ width: size, height: size }}
+      />
+    </div>
+  );
+}
